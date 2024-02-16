977f01f3d1244174a118b8f5d4ae90e2f494abc0
Hsun Tsai <hsuntsai0906@gmail.com>
2024-02-16 12:41:07 +0800

    add scss

---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import TheWelcome from '../../components/TheWelcome.vue'
+import './homePage.scss'
+</script>
+
+<template>
+	<div class="homePage">
+		<!-- <TheWelcome /> -->
+		123
+	</div>
+</template>
